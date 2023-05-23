c5896397b38f8c5c7d12eb568ddf22cab5ea93c7
nusuy <nsuy.ch@gmail.com>
2023-05-23 23:56:24 +0900

    chore: Add User entity (#1)

---
+++ src/main/java/com/swugether/server/db/domain/User.java
@@ -0,0 +1,23 @@
+package com.swugether.server.db.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long userId;
+  private String email;
+
+  public User(String email) {
+    this.email = email;
+  }
+}
