c91efefde84a45ca8251efa18e7d41add425e88d
nusuy <nsuy.ch@gmail.com>
2023-05-28 02:19:31 +0900

    feat: Add redis configuration (#1)

---
+++ src/main/java/com/swugether/server/db/dao/RefreshTokenRepository.java
@@ -0,0 +1,7 @@
+package com.swugether.server.db.dao;
+
+import com.swugether.server.db.domain.RefreshTokenEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RefreshTokenRepository extends CrudRepository<RefreshTokenEntity, Integer> {
+}
