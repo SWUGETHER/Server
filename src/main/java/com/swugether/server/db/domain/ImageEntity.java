4ada765ba5908c0ed1020302626a9b1c709182a8
Yusun Choi <nsuy.ch@gmail.com>
2023-05-31 11:51:48 +0900

    feat: Add entities (#1)

--- src/main/java/com/swugether/server/db/domain/ImageEntity.java
+++ src/main/java/com/swugether/server/db/domain/ImageEntity.java
@@ -1,34 +1,31 @@
 package com.swugether.server.db.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Lob;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+
+import javax.persistence.*;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "Image")
 public class ImageEntity {
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
-  @ManyToOne
-  private ContentEntity post;
-  @Lob
-  @NotNull
-  private String imagePath;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private ContentEntity post;
+    @Lob
+    private String imagePath;
 
-  @Builder
-  public ImageEntity(ContentEntity post, String imagePath) {
-    this.post = post;
-    this.imagePath = imagePath;
-  }
+    @Builder
+    public ImageEntity(ContentEntity post, String imagePath) {
+        this.post = post;
+        this.imagePath = imagePath;
+    }
+
+    public ImageEntity() {
+
+    }
 }
--- src/main/java/com/swugether/server/db/domain/UserEntity.java
+++ src/main/java/com/swugether/server/db/domain/UserEntity.java
@@ -6,13 +6,16 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "User")
+@NoArgsConstructor
 public class UserEntity {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,6 +25,7 @@ public class UserEntity {
   @NotNull
   private String nickname;
 
+  @Builder
   public UserEntity(String email, String nickname) {
     this.email = email;
     this.nickname = nickname;
