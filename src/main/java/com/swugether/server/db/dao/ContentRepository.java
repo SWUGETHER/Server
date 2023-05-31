a327831af65321aa5826bdddac777a7819267868
Yusun Choi <nsuy.ch@gmail.com>
2023-05-31 11:52:00 +0900

    feat: Add repositories (#1)

--- src/main/java/com/swugether/server/db/dao/ContentRepository.java
+++ src/main/java/com/swugether/server/db/dao/ContentRepository.java
@@ -1,7 +1,16 @@
 package com.swugether.server.db.dao;
 
 import com.swugether.server.db.domain.ContentEntity;
+import com.swugether.server.db.domain.UserEntity;
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ContentRepository extends CrudRepository<ContentEntity, Long> {
+  List<ContentEntity> findAllByUserOrderByCreatedAtDesc(UserEntity user);
+
+  List<ContentEntity> findAllByOrderByCreatedAtDesc();
+
+  List<ContentEntity> findAllByOrderByCreatedAtAsc();
+
+  List<ContentEntity> findAllByOrderByLikeCountDesc();
 }
--- src/main/java/com/swugether/server/db/dao/ImageRepository.java
+++ src/main/java/com/swugether/server/db/dao/ImageRepository.java
@@ -1,7 +1,12 @@
 package com.swugether.server.db.dao;
 
+import com.swugether.server.db.domain.ContentEntity;
 import com.swugether.server.db.domain.ImageEntity;
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ImageRepository extends CrudRepository<ImageEntity, Long> {
+  List<ImageEntity> findAllByPost(ContentEntity post);
+
+  ImageEntity findTopByPostOrderByIdAsc(ContentEntity post);
 }
