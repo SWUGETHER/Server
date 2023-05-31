74f66e8481344c9baa28ef923515dbf42f6d2a95
Yusun Choi <nsuy.ch@gmail.com>
2023-05-31 11:56:49 +0900

    fix: Modify codes (#1)
    - custom http status, ExceptionHandler

--- src/main/java/com/swugether/server/exception/ExceptionHandler.java
+++ src/main/java/com/swugether/server/exception/ExceptionHandler.java
@@ -4,6 +4,7 @@ import com.swugether.server.base.GeneralException;
 import com.swugether.server.base.constant.Code;
 import com.swugether.server.base.dto.ErrorResponseDto;
 import org.hibernate.exception.ConstraintViolationException;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,41 +15,40 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @RestControllerAdvice(annotations = {RestController.class})
 public class ExceptionHandler extends ResponseEntityExceptionHandler {
-  @org.springframework.web.bind.annotation.ExceptionHandler
-  public ResponseEntity<Object> validation(ConstraintViolationException e, WebRequest request) {
-    return handleExceptionInternal(e, Code.UNAUTHORIZED, request);
-  }
+    @org.springframework.web.bind.annotation.ExceptionHandler
+    public ResponseEntity<Object> validation(ConstraintViolationException e, WebRequest request) {
+        return handleExceptionInternal(e, Code.UNAUTHORIZED, request);
+    }
 
-  @org.springframework.web.bind.annotation.ExceptionHandler
-  public ResponseEntity<Object> general(GeneralException e, WebRequest request) {
-    return handleExceptionInternal(e, e.getErrCode(), request);
-  }
+    @org.springframework.web.bind.annotation.ExceptionHandler
+    public ResponseEntity<Object> general(GeneralException e, WebRequest request) {
+        return handleExceptionInternal(e, e.getErrCode(), request);
+    }
 
-  @org.springframework.web.bind.annotation.ExceptionHandler
-  public ResponseEntity<Object> exception(Exception e, WebRequest request) {
-    return handleExceptionInternal(e, Code.INTERNAL_ERROR, request);
-  }
+    @org.springframework.web.bind.annotation.ExceptionHandler
+    public ResponseEntity<Object> exception(Exception e, WebRequest request) {
+        return handleExceptionInternal(e, Code.INTERNAL_ERROR, request);
+    }
 
-  @Override
-  protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body,
-      HttpHeaders headers, HttpStatus status, WebRequest request) {
-    return handleExceptionInternal(ex, Code.valueOf(status), headers, status, request);
-  }
+    @Override
+    protected @NotNull ResponseEntity<Object> handleExceptionInternal(@NotNull Exception ex, Object body, @NotNull HttpHeaders headers, @NotNull HttpStatus status, @NotNull WebRequest request) {
+        return handleExceptionInternal(ex, Code.valueOf(status), headers, status, request);
+    }
 
-  private ResponseEntity<Object> handleExceptionInternal(Exception e, Code errorCode,
-      WebRequest request) {
-    return handleExceptionInternal(e, errorCode, HttpHeaders.EMPTY, errorCode.getHttpStatus(),
-        request);
-  }
+    private ResponseEntity<Object> handleExceptionInternal(Exception e, Code errorCode,
+                                                           WebRequest request) {
+        return handleExceptionInternal(e, errorCode, HttpHeaders.EMPTY, errorCode.getHttpStatus(),
+                request);
+    }
 
-  private ResponseEntity<Object> handleExceptionInternal(Exception e, Code errorCode,
-      HttpHeaders headers, HttpStatus status, WebRequest request) {
-    return super.handleExceptionInternal(
-        e,
-        ErrorResponseDto.of(errorCode, errorCode.getMessage(e)),
-        headers,
-        status,
-        request
-    );
-  }
+    private ResponseEntity<Object> handleExceptionInternal(Exception e, Code errorCode,
+                                                           HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return super.handleExceptionInternal(
+                e,
+                ErrorResponseDto.of(errorCode, errorCode.getMessage(e)),
+                headers,
+                status,
+                request
+        );
+    }
 }
