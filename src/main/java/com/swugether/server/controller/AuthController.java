b3d19e533c0f85910842eb0b763d533ad0b68a02
Yusun Choi <nsuy.ch@gmail.com>
2023-06-07 01:10:21 +0900

    feat: Modify AuthController (#1)

--- src/main/java/com/swugether/server/controller/AuthController.java
+++ src/main/java/com/swugether/server/controller/AuthController.java
@@ -8,21 +8,17 @@ import com.swugether.server.exception.UnauthorizedAccessException;
 import com.swugether.server.service.AuthService;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.InvalidClaimException;
-import java.io.IOException;
-import java.security.GeneralSecurityException;
-import java.util.Map;
-import javax.naming.NoPermissionException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.naming.NoPermissionException;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
@@ -30,104 +26,108 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 @Slf4j
 public class AuthController {
-  private AuthService authService;
+    private AuthService authService;
 
-  @Autowired
-  public AuthController(AuthService authService) {
-    this.authService = authService;
-  }
-
-  // 로그인
-  @PostMapping("/login")
-  public ResponseEntity<ResponseDto> login(@RequestHeader("id-token") String id_token) {
-    try {
-      Map<String, Object> data = authService.loginService(id_token);
-
-      return ResponseEntity.status(200).body(DataResponseDto.of(data));
-    } catch (GeneralSecurityException | IOException e) {
-      log.error(e.getMessage());
-
-      return ResponseEntity.status(401)
-          .body(ErrorResponseDto.of(Code.UNAUTHORIZED, "Invalid id token."));
-    } catch (Exception e) {
-      log.error(e.getMessage());
-
-      return ResponseEntity.status(500)
-          .body(ErrorResponseDto.of(Code.INTERNAL_ERROR, "Server error."));
+    @Autowired
+    public AuthController(AuthService authService) {
+        this.authService = authService;
     }
-  }
 
-  // 로그아웃
-  @PostMapping("/logout")
-  public ResponseEntity<ResponseDto> logout(@RequestHeader("Authorization") String bearer_token)
-      throws IndexOutOfBoundsException, UnauthorizedAccessException, EmptyResultDataAccessException {
-    try {
-      authService.logoutService(bearer_token);
+    // 로그인
+    @PostMapping("/login")
+    public ResponseEntity<ResponseDto> login(@RequestHeader("id-token") String id_token) {
+        try {
+            Map<String, Object> data = authService.loginService(id_token);
 
-      return ResponseEntity.ok(ResponseDto.of(Code.OK));
-    } catch (IndexOutOfBoundsException | InvalidClaimException | ExpiredJwtException e) {
-      log.error(e.getMessage());
+            return ResponseEntity.status(200).body(DataResponseDto.of(data));
+        } catch (GeneralSecurityException | IOException e) {
+            log.error(e.getMessage());
 
-      return ResponseEntity.status(401)
-          .body(ErrorResponseDto.of(Code.UNAUTHORIZED, "Invalid Access token."));
-    } catch (UnauthorizedAccessException | EmptyResultDataAccessException e) {
-      log.error(e.getMessage());
-      return ResponseEntity.status(401)
-          .body(ErrorResponseDto.of(Code.UNAUTHORIZED, e.getMessage()));
-    } catch (Exception e) {
-      log.error(e.getMessage());
-      return ResponseEntity.status(500)
-          .body(ErrorResponseDto.of(Code.INTERNAL_ERROR, "Internal server error"));
+            return ResponseEntity.status(401)
+                    .body(ErrorResponseDto.of(Code.UNAUTHORIZED, "Invalid id token."));
+        } catch (Exception e) {
+            log.error(e.getMessage());
+
+            return ResponseEntity.status(500)
+                    .body(ErrorResponseDto.of(Code.INTERNAL_ERROR, "Server error."));
+        }
     }
-  }
 
-  // 회원탈퇴
-  @PostMapping("/leave")
-  public ResponseEntity<ResponseDto> leave(@RequestHeader("Authorization") String bearer_token) {
-    try {
-      authService.leaveService(bearer_token);
+    // 로그아웃
+    @PostMapping("/logout")
+    public ResponseEntity<ResponseDto> logout(@RequestHeader("Authorization") String bearer_token)
+            throws IndexOutOfBoundsException, UnauthorizedAccessException, EmptyResultDataAccessException {
+        try {
+            authService.logoutService(bearer_token);
 
-      return ResponseEntity.ok(ResponseDto.of(Code.OK));
-    } catch (IndexOutOfBoundsException | InvalidClaimException | ExpiredJwtException e) {
-      log.error(e.getMessage());
+            return ResponseEntity.ok(ResponseDto.of(Code.OK));
+        } catch (IndexOutOfBoundsException | InvalidClaimException | ExpiredJwtException e) {
+            log.error(e.getMessage());
 
-      return ResponseEntity.status(401)
-          .body(ErrorResponseDto.of(Code.UNAUTHORIZED, "Invalid Access token."));
-    } catch (UnauthorizedAccessException | EmptyResultDataAccessException e) {
-      log.error(e.getMessage());
-      return ResponseEntity.status(401)
-          .body(ErrorResponseDto.of(Code.UNAUTHORIZED, e.getMessage()));
-    } catch (Exception e) {
-      log.error(e.getMessage());
-      return ResponseEntity.status(500)
-          .body(ErrorResponseDto.of(Code.INTERNAL_ERROR, "Internal server error"));
+            return ResponseEntity.status(401)
+                    .body(ErrorResponseDto.of(Code.UNAUTHORIZED, "Invalid Access token."));
+        } catch (UnauthorizedAccessException | EmptyResultDataAccessException e) {
+            log.error(e.getMessage());
+            return ResponseEntity.status(401)
+                    .body(ErrorResponseDto.of(Code.UNAUTHORIZED, e.getMessage()));
+        } catch (NoPermissionException e) {
+            log.error(e.getMessage());
+            return ResponseEntity.status(403)
+                    .body(ErrorResponseDto.of(Code.FORBIDDEN, e.getMessage()));
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return ResponseEntity.status(500)
+                    .body(ErrorResponseDto.of(Code.INTERNAL_ERROR, "Internal server error"));
+        }
     }
-  }
 
-  // AccessToken 재발급
-  @GetMapping("/refresh")
-  public ResponseEntity<ResponseDto> refresh(@RequestHeader("Authorization") String bearer_token,
-      @RequestHeader("Refresh") String bearer_refresh)
-      throws IndexOutOfBoundsException, InvalidClaimException, ExpiredJwtException, UnauthorizedAccessException {
-    try {
-      return ResponseEntity.status(200)
-          .body(DataResponseDto.of(authService.refreshService(bearer_token, bearer_refresh)));
-    } catch (NoPermissionException e) {
-      log.error(e.getMessage());
+    // 회원탈퇴
+    @PostMapping("/leave")
+    public ResponseEntity<ResponseDto> leave(@RequestHeader("Authorization") String bearer_token) {
+        try {
+            authService.leaveService(bearer_token);
 
-      return ResponseEntity.status(403)
-          .body(ErrorResponseDto.of(Code.FORBIDDEN, e.getMessage()));
-    } catch (IndexOutOfBoundsException | InvalidClaimException | ExpiredJwtException |
-             UnauthorizedAccessException e) {
-      log.error(e.getMessage());
+            return ResponseEntity.ok(ResponseDto.of(Code.OK));
+        } catch (IndexOutOfBoundsException | InvalidClaimException | ExpiredJwtException e) {
+            log.error(e.getMessage());
 
-      return ResponseEntity.status(401)
-          .body(ErrorResponseDto.of(Code.UNAUTHORIZED, e.getMessage()));
-    } catch (Exception e) {
-      log.error(e.getMessage());
-
-      return ResponseEntity.status(500)
-          .body(ErrorResponseDto.of(Code.INTERNAL_ERROR, "Server error."));
+            return ResponseEntity.status(401)
+                    .body(ErrorResponseDto.of(Code.UNAUTHORIZED, "Invalid Access token."));
+        } catch (UnauthorizedAccessException | EmptyResultDataAccessException e) {
+            log.error(e.getMessage());
+            return ResponseEntity.status(401)
+                    .body(ErrorResponseDto.of(Code.UNAUTHORIZED, e.getMessage()));
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return ResponseEntity.status(500)
+                    .body(ErrorResponseDto.of(Code.INTERNAL_ERROR, "Internal server error"));
+        }
     }
-  }
+
+    // AccessToken 재발급
+    @GetMapping("/refresh")
+    public ResponseEntity<ResponseDto> refresh(@RequestHeader("Authorization") String bearer_token,
+                                               @RequestHeader("Refresh") String bearer_refresh)
+            throws IndexOutOfBoundsException, InvalidClaimException, ExpiredJwtException, UnauthorizedAccessException {
+        try {
+            return ResponseEntity.status(200)
+                    .body(DataResponseDto.of(authService.refreshService(bearer_token, bearer_refresh)));
+        } catch (NoPermissionException e) {
+            log.error(e.getMessage());
+
+            return ResponseEntity.status(403)
+                    .body(ErrorResponseDto.of(Code.FORBIDDEN, e.getMessage()));
+        } catch (IndexOutOfBoundsException | InvalidClaimException | ExpiredJwtException |
+                 UnauthorizedAccessException e) {
+            log.error(e.getMessage());
+
+            return ResponseEntity.status(401)
+                    .body(ErrorResponseDto.of(Code.UNAUTHORIZED, e.getMessage()));
+        } catch (Exception e) {
+            log.error(e.getMessage());
+
+            return ResponseEntity.status(500)
+                    .body(ErrorResponseDto.of(Code.INTERNAL_ERROR, "Server error."));
+        }
+    }
 }
