de58d07af32ef699f80406d284861d596d792d60
Yusun Choi <nsuy.ch@gmail.com>
2023-06-07 01:13:06 +0900

    feat: Modify Repositories (#1)

--- src/main/java/com/swugether/server/db/dao/ImageRepository.java
+++ src/main/java/com/swugether/server/db/dao/ImageRepository.java
@@ -2,11 +2,21 @@ package com.swugether.server.db.dao;
 
 import com.swugether.server.db.domain.ContentEntity;
 import com.swugether.server.db.domain.ImageEntity;
-import java.util.List;
+import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ImageRepository extends CrudRepository<ImageEntity, Long> {
-  List<ImageEntity> findAllByPost(ContentEntity post);
+import javax.transaction.Transactional;
+import java.util.List;
 
-  ImageEntity findTopByPostOrderByIdAsc(ContentEntity post);
+public interface ImageRepository extends CrudRepository<ImageEntity, Long> {
+    List<ImageEntity> findAllByPost(ContentEntity post);
+
+    ImageEntity findTopByPostOrderByIdAsc(ContentEntity post);
+
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query(value = "UPDATE ImageEntity i SET i.imagePath = :imagePath WHERE i.id = :id")
+    void updateImagePath(@Param("imagePath") String imagePath, @Param("id") Long id);
 }
