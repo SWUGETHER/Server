c494ff54094186137962dba29069cea24341c63c
Yusun Choi <nsuy.ch@gmail.com>
2023-09-19 15:02:10 +0900

    refactor: Modify OCR features (#3)

--- src/main/java/com/swugether/server/domain/OCR/api/OcrController.java
+++ src/main/java/com/swugether/server/domain/OCR/api/OcrController.java
@@ -1,22 +1,20 @@
 package com.swugether.server.domain.OCR.api;
 
 import com.swugether.server.domain.OCR.application.OcrService;
-import com.swugether.server.global.base.constant.Code;
 import com.swugether.server.global.base.dto.DataResponseDto;
-import com.swugether.server.global.base.dto.ErrorResponseDto;
 import com.swugether.server.global.base.dto.ResponseDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Objects;
-
-@RestController
-@ResponseBody
-@RequestMapping("/image")
 @Slf4j
+@RestController
+@RequestMapping("/image")
 public class OcrController {
     private final OcrService ocrService;
 
@@ -28,20 +26,10 @@ public class OcrController {
     // 이미지 인식 및 텍스트 추출
     @PostMapping
     public ResponseEntity<ResponseDto> textFromImage(@RequestPart(value = "file") MultipartFile image) {
-        try {
-            String result = ocrService.textFromImageService(image);
 
-            return ResponseEntity.status(200).body(DataResponseDto.of(result));
-        } catch (Exception e) {
-            log.error(e.getMessage());
+        String result = ocrService.textFromImageService(image);
 
-            if (Objects.equals(e.getMessage(), "Type error. (Image required.)")) {
-                return ResponseEntity.status(400)
-                        .body(ErrorResponseDto.of(Code.BAD_REQUEST, e.getMessage()));
-            }
+        return ResponseEntity.status(201).body(DataResponseDto.of(result, 201));
 
-            return ResponseEntity.status(500)
-                    .body(ErrorResponseDto.of(Code.INTERNAL_ERROR, e.getMessage()));
-        }
     }
 }
--- src/main/java/com/swugether/server/domain/OCR/application/OcrService.java
+++ src/main/java/com/swugether/server/domain/OCR/application/OcrService.java
@@ -1,6 +1,9 @@
 package com.swugether.server.domain.OCR.application;
 
+import com.swugether.server.domain.OCR.exception.OcrErrorCode;
+import com.swugether.server.global.exception.GlobalException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -16,49 +19,41 @@ import java.util.Objects;
 @Service
 @Slf4j
 public class OcrService {
+    @Value("${FAST_API_BASE_URL}")
+    private String API_BASE_URL;
+
     // fast api 요청
-    public String postFile(MultipartFile image) throws Exception {
-        String apiUrl = System.getenv("FAST_API_BASE_URL") + "/extract_data";
+    public String textFromImageService(MultipartFile image) throws GlobalException {
+        String url = API_BASE_URL + "/extract_data";
 
-        try {
-            // 헤더 설정
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+        // 헤더 설정
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
 
-            // 요청 본문 설정
-            MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
-            body.add("file", image.getResource());
+        // 요청 본문 설정
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("file", image.getResource());
 
-            // HTTP 요청 엔티티 생성
-            HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+        // HTTP 요청 엔티티 생성
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
 
-            // RestTemplate을 사용하여 FastAPI로 POST 요청 보내기
-            RestTemplate restTemplate = new RestTemplate();
-            ResponseEntity<String> response = restTemplate.postForEntity(apiUrl, requestEntity, String.class);
+        // RestTemplate을 사용하여 FastAPI로 POST 요청 보내기
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.postForEntity(url, requestEntity, String.class);
 
-            // 응답 처리
-            if (response.getStatusCode().is2xxSuccessful()) {
-                log.info("[ " + apiUrl + " ]" + " Request successed.");
+        // 응답 처리
+        if (response.getStatusCode().is2xxSuccessful()) {
+            log.info("[ " + url + " ]" + " Request successed.");
 
-                return response.getBody();
-            } else if (response.getStatusCode().value() == 422 && Objects.requireNonNull(response.getBody()).contains("value_error")) {
-                log.error("Type error.");
+            return response.getBody();
+        } else if (response.getStatusCode().value() == 422 && Objects.requireNonNull(response.getBody()).contains("value_error")) {
+            log.error("Type error.");
 
-                throw new IllegalArgumentException("Type error. (Image required.)");
-            } else {
-                log.error("[ " + apiUrl + " ]" + " Request failed.");
+            throw new GlobalException(OcrErrorCode.INVALID_IMAGE_FORMAT);
+        } else {
+            log.error("[ " + url + " ]" + " Request failed.");
 
-                throw new Exception("Request failed.");
-            }
-        } catch (Exception e) {
-            log.error(e.getMessage());
-            throw e;
+            throw new GlobalException(OcrErrorCode.API_REQUEST_FAILED);
         }
-    }
-
-    // 이미지 인식
-    public String textFromImageService(MultipartFile file) throws Exception {
-
-        return postFile(file);
     }
 }
